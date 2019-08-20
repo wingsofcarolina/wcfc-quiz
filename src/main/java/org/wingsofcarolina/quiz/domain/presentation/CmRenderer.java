d3d5d89fbe00fad95a9c8f4e1b40118d4c9d1ea5
Dwight Frye <dwight@openweave.org>
2019-08-20 09:21:56 -0400

    Interum rendering changes

--- src/main/java/org/wingsofcarolina/quiz/domain/presentation/CmRenderer.java
+++ src/main/java/org/wingsofcarolina/quiz/domain/presentation/CmRenderer.java
@@ -3,16 +3,38 @@ package org.wingsofcarolina.quiz.domain.presentation;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CmRenderer {
+	private static final Logger LOG = LoggerFactory.getLogger(CmRenderer.class);
+
 	private static Parser parser = Parser.builder().build();
 	private static HtmlRenderer renderer = HtmlRenderer.builder().build();
 
 	public static String render(String input) {
 		Node document = parser.parse(input);
 		String output = renderer.render(document);
-		int first = output.indexOf('>');
-		int last = output.lastIndexOf('<');
-		return output.substring(first+1,last-1);
+//		LOG.info(">>>>>>>> {}", output.trim());
+//		int last = output.lastIndexOf('<');
+//		String result = output.substring(3);
+//		LOG.info("<<<<<<<< {}", result);
+		String result = remove(output, "<p>");
+		result = remove(result, "</p>");
+		return result;
+	}
+	
+	public static String remove(String input, String cut) {
+	    StringBuilder sb = new StringBuilder(input);
+	    if(input.length() < cut.length()){
+	       return input;
+	    }
+	    else if(input.substring(1, 4).equals("del")){
+	        StringBuilder afterRemove = sb.delete(1, 4);
+	        return afterRemove.toString();
+	    }
+	    else{
+	       return input;
+	    }
 	}
 }
