d8b8f00084778e1a9b9e7b78e3976c45aa0686f2
Graham Mainwaring <graham@mhn.org>
2025-09-29 18:14:10 -0400

    Remove obsolete deployment files

--- kubernetes/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kubectl apply -f wcfc-quiz.yaml
-export NODE_PORT=$(kubectl get services/wcfc-quiz -o go-template='{{(index .spec.ports 0).nodePort}}')
-echo http://$(minikube ip):$NODE_PORT
--- kubernetes/teardown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl delete service wcfc-quiz
-kubectl delete deployment wcfc-quiz
-kubectl delete service mongodb
-kubectl delete sts mongodb
